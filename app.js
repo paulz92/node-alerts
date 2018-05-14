10c89a330bef36220dfb1f3a43b5e8e58ea8d9ee
Paul Zavattieri <paulz92@gmail.com>
2018-05-13 20:32:05 -0400

    swapped out insulter for pokemon alerts - api integration to retrieve pokemon

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const notifier = require('node-notifier');
-const InsultCompliment = require("insult-compliment");
+const request = require('request');
+
+////////// comment this in/pokemon out for notifications that either insult or compliment you each minute
+// const InsultCompliment = require("insult-compliment");
+
+// const notify = () => {
+//   let action;
+//   let randomNum = Math.floor(Math.random() * 100);
+
+//   if (randomNum > 30) {
+//     action = InsultCompliment.Insult();
+//   } else {
+//     action = InsultCompliment.Compliment();
+//   }
+  
+//   notifier.notify({
+//     title: 'Hey you',
+//     message: action
+//   });
+// }
 
 const notify = () => {
-  let action;
+  const pokeNum = Math.floor(Math.random() * 151) + 1;
+  const apiURL = 'https://www.pokeapi.co/api/v2/pokemon-form/' + pokeNum + '/';
 
-  let randomNum = Math.floor(Math.random() * 2);
-
-  if (randomNum === 0) {
-    action = InsultCompliment.Insult();
-  } else {
-    action = InsultCompliment.Compliment();
-  }
-  
-  notifier.notify({
-    title: 'Hey you',
-    message: action
+  request(apiURL, function (err, res, body) {
+    if (err) throw err;
+    if (res.statusCode === 200) {
+      notifier.notify({
+        title: 'Pokemon of the Minute',
+        message: JSON.parse(body).name
+      });
+    }
   });
 }
 
-setInterval(notify, 5000);
+setInterval(notify, 60000);
